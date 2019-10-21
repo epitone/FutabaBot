ef65924cb409e6d4c8ed3eecf7ce07415e4187d7
Tarik Massac <tarik.massac@gmail.com>
2019-10-21 18:03:59 -0400

    added node-opus dependency

--- FutabaBot.js
+++ FutabaBot.js
@@ -1,7 +1,22 @@
+const fs = require('fs');
 const Discord = require('discord.js');
+
 //TODO: make prefix a var and salt token
 const { prefix, token } = require("./config.json"); // see: object destructuring
 const client = new Discord.Client();
+
+client.commands = new Discord.Collection();
+const cooldowns = new Discord.Collection();
+
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// set a new item in the Collection
+	// with the key as the command name and the value as the exported module
+	client.commands.set(command.name, command);
+}
+
 
 
 // fires off when client is ready
@@ -10,59 +25,60 @@ client.once('ready', () => {
 });
 
 client.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+    // if (!message.content.startsWith(prefix) || message.author.bot) return;
+	// if (!message.channel.permissionsFor(client.user).has('SEND_MESSAGES')) return; // if logged-in user cannnot send messages
+
+    // const botPerms = ['MANAGE_MESSAGES', 'KICK_MEMBERS', 'MANAGE_ROLES', 'MANAGE_CHANNELS'];
+
+    // if (!message.guild.me.permissions.has(botPerms)) {
+	// 	return message.reply(`I need the permissions ${botPerms.join(', ')} for this demonstration to work properly`); // if bot does not have proper permissions
+	// }
 
 	const args = message.content.slice(prefix.length).split(/ +/);
-	const command = args.shift().toLowerCase();
+    const commandName = args.shift().toLowerCase();
 
-	if (command === 'ping') {
-		message.channel.send('Pong.');
-	} else if (command === 'beep') {
-		message.channel.send('Boop.');
-	} else if (command === 'server') {
-		message.channel.send(`Server name: ${message.guild.name}\nTotal members: ${message.guild.memberCount}`);
-	} else if (command === 'user-info') {
-		message.channel.send(`Your username: ${message.author.username}\nYour ID: ${message.author.id}`);
-	} else if (command === 'info') {
-		if (!args.length) {
-			return message.channel.send(`You didn't provide any arguments, ${message.author}!`);
-		} else if (args[0] === 'foo') {
-			return message.channel.send('bar');
-		}
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
 
-		message.channel.send(`First argument: ${args[0]}`);
-	} else if (command === 'kick') {
-		if (!message.mentions.users.size) {
-			return message.reply('you need to tag a user in order to kick them!');
-		}
+    if (!command) return;
 
-		const taggedUser = message.mentions.users.first();
+    if (command.guildOnly && message.channel.type !== 'text') {
+        return message.reply('I can\'t execute that command inside DMs!');
+    }
 
-		message.channel.send(`You wanted to kick: ${taggedUser.username}`);
-	} else if (command === 'avatar') {
-		if (!message.mentions.users.size) {
-			return message.channel.send(`Your avatar: <${message.author.displayAvatarURL()}>`);
-		}
+    if (command.args && !args.length) {
+        let reply = `You didn't provide any arguments, ${message.author}!`;
 
-		const avatarList = message.mentions.users.map(user => {
-			return `${user.username}'s avatar: <${user.displayAvatarURL()}>`;
-		});
+        if(command.usage) {
+            reply += `The proper usage would be: \`${prefix}${command.name} ${command.usage}\``;
+        }
+    }
 
-		message.channel.send(avatarList);
-	} else if (command === 'prune') {
-		const amount = parseInt(args[0]) + 1;
+    if (!cooldowns.has(command.name)) { // check if command has cooldown
+        cooldowns.set(command.name, new Discord.Collection()); // if so, put it in the cooldowns Collection
+    }
 
-		if (isNaN(amount)) {
-			return message.reply('that doesn\'t seem to be a valid number.');
-		} else if (amount <= 1 || amount > 100) {
-			return message.reply('you need to input a number between 1 and 99.');
-		}
+    const now = Date.now(); // current DateTime
+    const timestamps = cooldowns.get(command.name); 
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+    
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
 
-		message.channel.bulkDelete(amount, true).catch(err => {
-			console.error(err);
-			message.channel.send('there was an error trying to prune messages in this channel!');
-		});
-	}
+        if (now < expirationTime) { // if the command cooldown hasn't expired
+            const timeLeft = (expirationTime - now) / 1000; // calculate the time left (in seconds)
+            return message.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${command.name}\` command.`);
+        }
+    } else {
+        timestamps.set(message.author.id, now);
+        setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+    }
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('there was an error trying to execute that command!');
+    }
 });
 
 // login with app token
