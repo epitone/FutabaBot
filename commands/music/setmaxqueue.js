e905cf918974c3010860a9aa3e32772e08d4e1c7
tai m <secondubly@gmail.com>
2020-05-14 09:50:54 -0400

    add setmaxqueue alias

--- commands/music/setmaxqueue.js
+++ commands/music/setmaxqueue.js
@@ -5,6 +5,7 @@ module.exports = class SetMaxQueueCommand extends Command {
   constructor (client) {
     super(client, {
       name: 'setmaxqueue',
+      aliases: ['smq'],
       group: 'music', // the command group the command is a part of.
       memberName: 'setmaxqueue', // the name of the command within the group (this can be different from the name).
       description: 'Sets the maximum queue size. Specify no parameters to have no limit.', // TODO should this persist between sessions?
