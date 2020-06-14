051c2f266381a4879a561c184bc28ce99ac06bfa
tai m <secondubly@gmail.com>
2020-06-14 19:29:01 -0400

    updated discord utils

--- utils/discord-utils.js
+++ utils/discord-utils.js
@@ -1,41 +1,43 @@
-const { MessageEmbed, User } = require('discord.js')
+const { MessageEmbed, Permissions } = require('discord.js')
 
-exports.embedResponse = (message, embedOptions, textChannel = null) => {
-  const embed = new MessageEmbed()
-  if (embedOptions.color) embed.setColor(embedOptions.color)
-  if (embedOptions.title) embed.setTitle(embedOptions.title)
-  if (embedOptions.description) embed.setDescription(embedOptions.description)
-  if (embedOptions.url) embed.setURL(embedOptions.url)
-  if (embedOptions.author) embed.setAuthor(embedOptions.author)
-  if (embedOptions.footer) embed.setFooter(embedOptions.footer)
-  if (embedOptions.fields) {
-    for (const field of embedOptions.fields) {
-      embed.addFields({
-        name: field.name,
-        value: field.value,
-        inline: field.inline ? field.inline : false
-      })
+module.exports = {
+  embedResponse (message, embedOptions, textChannel = null) {
+    const embed = new MessageEmbed()
+    if (embedOptions.color) embed.setColor(embedOptions.color)
+    if (embedOptions.title) embed.setTitle(embedOptions.title)
+    if (embedOptions.description) embed.setDescription(embedOptions.description)
+    if (embedOptions.url) embed.setURL(embedOptions.url)
+    if (embedOptions.author) embed.setAuthor(embedOptions.author)
+    if (embedOptions.footer) embed.setFooter(embedOptions.footer)
+    if (embedOptions.fields) {
+      for (const field of embedOptions.fields) {
+        embed.addFields({
+          name: field.name,
+          value: field.value,
+          inline: field.inline ? field.inline : false
+        })
+      }
     }
-  }
-  if (textChannel) {
-    textChannel.send(embed)
-    return
-  }
-  message.embed(embed)
-}
+    if (textChannel) {
+      textChannel.send(embed)
+      return
+    }
+    message.embed(embed)
+  },
 
-exports.inVoiceChannel = (voiceState, message, response = null) => {
-  if (!voiceState.channel) {
-    this.embedResponse(message, {
-      color: 'RED',
-      description: response === null ? 'You need to be in a voice channel to use this command.' : response
-    })
-    console.warn(`${message.author.tag} attempted to run a music command without being in a voice channel.`)
-    return false
-  }
-  return true
-}
+  inVoiceChannel (voiceState, message, response = null) {
+    if (!voiceState.channel) {
+      this.embedResponse(message, {
+        color: 'RED',
+        description: response === null ? 'You need to be in a voice channel to use this command.' : response
+      })
+      console.warn(`${message.author.tag} attempted to run a music command without being in a voice channel.`)
+      return false
+    }
+    return true
+  },
 
-exports.isAdminOrHasPerms = (user, permissionRole) => {
-  return user.hasPermisssion('ADMINISTRATOR') || user.hasPermission(permissionRole)
+  isAdminOrHasPerms (user, permissionRole) {
+    return user.hasPermisssion(Permissions.FLAGS.ADMINISTRATOR) || user.hasPermission(permissionRole)
+  }
 }
