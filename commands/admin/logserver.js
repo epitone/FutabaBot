0f8c7cc828e05f816c232fccdffc96b96c772cc5
Tai M <secondubly@gmail.com>
2020-07-11 12:51:52 -0400

    fixed logging message

--- commands/admin/logserver.js
+++ commands/admin/logserver.js
@@ -3,7 +3,7 @@ Author: epitone
 logserver.js (c) 2020
 Desc: Enables/disables all logging events for a server - when enabled, all log events will output to the channel the command was executed in.
 Created:  2020-07-11T15:41:34.002Z
-Modified: 2020-07-11T15:58:21.994Z
+Modified: 2020-07-11T16:11:47.956Z
 */
 
 const { Command } = require('discord.js-commando')
@@ -41,7 +41,7 @@ module.exports = class LogServerCommand extends Command {
     }
 
     if (!discordUtils.hasPerms(message.guild.me, 'ADMINISTRATOR')) {
-      winston.warn(`${this.client.user.tag} does not have the \`MANAGE_GUILD\` permission`)
+      winston.warn(`${this.client.user.tag} does not have the \`ADMINISTRATOR\` permission`)
       discordUtils.embedResponse(message, {
         color: 'RED',
         description: constants.get('ERR_MISSING_BOT_PERMS', message.author, 'ADMINISTRATOR')
