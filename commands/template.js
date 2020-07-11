eb2bad628740d09840766de47a73fb7d96f0a16b
Tai M <secondubly@gmail.com>
2020-07-11 12:08:02 -0400

    template.js now adheres to standardjs rules

--- commands/template.js
+++ commands/template.js
@@ -1,12 +1,12 @@
-// const { Command } = require('discord.js-commando');
+// const { Command } = require('discord.js-commando')
 
 // module.exports = class TemplateCommand extends Command {
 //   constructor (client) {
 //     super(client, {
 //       name: 'commandname',
 //       aliases: ['aliases', 'go', 'here'],
-//       group: 'command-group', //the command group the command is a part of.
-//       memberName: 'commandname', //the name of the command within the group (this can be different from the name).
+//       group: 'command-group', // the command group the command is a part of.
+//       memberName: 'commandname', // the name of the command within the group (this can be different from the name).
 //       description: 'Description of command.',
 //       args: [
 //         {
