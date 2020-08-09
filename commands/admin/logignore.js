d471724e2ccbe04b5dd18f1b02cc8938cd86fb96
Tai M <secondubly@gmail.com>
2020-08-08 21:06:29 -0400

    reworked logignore command

--- commands/admin/logignore.js
+++ commands/admin/logignore.js
@@ -3,7 +3,7 @@ Author: epitone
 logignore.js (c) 2020
 Desc: Toggles whether the logserver command ignores this channel.
 Created:  2020-07-11T16:08:27.739Z
-Modified: 2020-07-12T03:37:36.906Z
+Modified: 2020-08-09T01:00:32.690Z
 */
 
 const { Command } = require('discord.js-commando')
@@ -41,31 +41,20 @@ module.exports = class LogIgnoreCommand extends Command {
       return
     }
 
-    const adminService = require('./../../FutabaBot').getAdminService()
-    const ignoreList = adminService.getLogIgnoreList(message.guild)
-    if (ignoreList.includes(message.channel.id)) {
-      const removedValue = adminService.removeFromLogIgnoreList(message.guild, message.channel)
-      if (!removedValue) {
+    const logService = require('./../../FutabaBot').getLogService()
+    const result = logService.logIgnore(message.guild, message.channel)
+    if (result) {
+      if (Array.isArray(result)) { // row was deleted
         discordUtils.embedResponse(message, {
-          color: 'RED',
-          description: constants.get('ERR_GENERIC')
+          color: 'ORANGE',
+          description: constants.get('LOG_CHANNEL_REMOVED', message.channel)
         })
-        return
+      } else { // channel was removed
+        discordUtils.embedResponse(message, {
+          color: 'ORANGE',
+          description: constants.get('LOG_CHANNEL_ADDED', message.channel)
+        })
       }
-      if (removedValue.length === 1) {
-        if (removedValue[0] === message.channel.id) {
-          discordUtils.embedResponse(message, {
-            color: 'ORANGE',
-            description: constants.get('LOG_CHANNEL_REMOVED', message.channel)
-          })
-        }
-      }
-    } else {
-      adminService.addToLogIgnoreList(message.guild, message.channel)
-      discordUtils.embedResponse(message, {
-        color: 'ORANGE',
-        description: constants.get('LOG_CHANNEL_ADDED', message.channel)
-      })
     }
   }
 }
